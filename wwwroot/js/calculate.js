a42adc4d9933f06fad7639dab81efd0a38710bd3
audreyseager <audreyjy@byu.edu>
2022-01-07 15:34:08 -0700

    * index.html: * favicon.jpeg: * bootstrap.js: * bootstrap.css: * bootstrap.js.map: * bootstrap.esm.js: * bootstrap.min.js: * bootstrap.css.map: * bootstrap.rtl.css: * bootstrap.min.css: * bootstrap-grid.css: * bootstrap.bundle.js: * bootstrap.esm.min.js: * bootstrap.esm.js.map: * bootstrap.min.js.map: * bootstrap-reboot.css: * bootstrap.rtl.css.map: * bootstrap.rtl.min.css: * bootstrap.min.css.map: * bootstrap.bundle.js.map: * bootstrap.bundle.min.js: * bootstrap-grid.rtl.css: * bootstrap-grid.min.css: * bootstrap-grid.css.map: * bootstrap.esm.min.js.map: * bootstrap-utilities.css: * bootstrap-reboot.rtl.css: * bootstrap-reboot.min.css: * bootstrap-reboot.css.map: * bootstrap.rtl.min.css.map: * bootstrap-grid.rtl.min.css: * bootstrap.bundle.min.js.map: * bootstrap-grid.rtl.css.map: * bootstrap-grid.min.css.map: * bootstrap-utilities.css.map: * bootstrap-utilities.min.css: * bootstrap-utilities.rtl.css: * bootstrap-reboot.rtl.min.css: * bootstrap-reboot.rtl.css.map: * bootstrap-reboot.min.css.map: * bootstrap-grid.rtl.min.css.map: * bootstrap-utilities.rtl.min.css: * bootstrap-utilities.min.css.map: * bootstrap-utilities.rtl.css.map: * bootstrap-reboot.rtl.min.css.map: * bootstrap-utilities.rtl.min.css.map:
    * Mission2.csproj:
    * calculate.js: commit 2

--- wwwroot/js/calculate.js
+++ wwwroot/js/calculate.js
@@ -1,86 +1,64 @@
 ﻿
+// function that calculates the percentage and letter grade for the course then alerts the user
 $("#button").click(function () {
-    alert("Based on the input, your overall percentage is : % and your letter grade is a(n)");
+
+    var grade;
+
+    // assigning input to variables and parsing to an integer so they can be added 
+    var a = parseFloat($("#assignments").val()) 
+    var g = parseFloat($("#group").val())
+    var q = parseFloat($("#quizzes").val())
+    var e = parseFloat($("#exams").val())
+    var i = parseFloat($("#intex").val())
+
+    // adding inputs together 
+    var x = a + g + q + e + i
+
+
+    // if statement that reads the sum of the inputs and determines the letter grade 
+    if
+        (x >= 94) {
+        grade = 'A';
+    }
+    else
+        if (x >= 90) {
+            grade = 'A-';
+        }
+        else if (x >= 87) {
+            grade = 'B+';
+        }
+        else if (x >= 84) {
+            grade = 'B';
+        }
+        else if (x >= 80) {
+            grade = 'B-';
+        }
+        else if (x >= 77) {
+            grade = 'C+';
+        }
+        else if (x >= 74) {
+            grade = 'C';
+        }
+        else if (x >= 70) {
+            grade = 'C-';
+        }
+        else if (x >= 67) {
+            grade = 'D+';
+        }
+        else if (x >= 64) {
+            grade = 'D';
+        }
+        else if (x >= 60) {
+            grade = 'D-';
+        }
+        else if (x < 60) {
+            grade = 'E';
+        }
+
+    // alert stating the percentage and letter grade 
+    alert("Based on the input, your overall percentage is: " + x + "% and your letter grade is a(n) " + grade);
 })
 
 
 
-var grade 
 
-var x = (($("#assignments").val()
-    + $("#group").val()
-    + $("#quizzes").val()
-    + $("#exams").val()
-    + $("#intex").val() / 100))
-
-
-if
-        x >= 94 {
-    grade = 'A';
-}
-else
-    if 90 <= x < 94 {
-        grade = 'A-'
-    }
-    else if 87 <= x < 90 {
-        grade = 'B+'
-    }
-    else if 84 <= x < 87 {
-        grade = 'B'
-    }
-    else if 80 <= x < 84 % {
-        grade = 'B-'
-    }
-    else if 77 <= x < 80 {
-        grade = 'C+'
-    }
-    else if 74 <= x < 77 {
-        grade = 'C'
-    }
-    else if 70 <= x < 74 {
-        grade = 'C-'
-    }
-    else if 67 <= x < 70 {
-        grade = 'D+'
-    }
-    else if 64 <= x < 67 {
-        grade = 'D'
-    }
-    else if 60 <= x < 64 {
-        grade = 'D-'
-    }
-    else if x < 60 {
-        grade = 'E'
-    }
-
-
-
-//74 % <= Total < 77 % ...C
-//90 % <= Total < 94 % ...A -
-//70 % <= Total < 74 % ...C-
-//87 % <= Total < 90 % ...B +
-//67 % <= Total < 70 % ...D +
-//84 % <= Total < 87 % ...B
-//64 % <= Total < 67 % ...D
-//80 % <= Total < 84 % ...B -
-//60 % <= Total < 64 % ...D- 
-//77 % <= Total < 80 % ...C+
-//Total < 60 % ...E
-
-
-
-//document.getElementById("btnSend").addEventListener("click", function () {
-//    alert("From: " + document.getElementById("txtFrom").value +
-//        ", Subject: " + document.getElementById("txtSubject").value +
-//        ", Message: " + document.getElementById("txtMsg").value);
-//})
-
-
-
-        //+ ($("assignments").val() * 100 / 55)
-        //+ ($("group").val() * 100 / 5)
-        //+ ($("quizzes").val() * 100 / 10)
-        //+ ($("exams").val() * 100 / 20)
-        //+ ($("intex").val() * 100 / 10)
-        //+ " and your percentage in the class is: "
-        //+
